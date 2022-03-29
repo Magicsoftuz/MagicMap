cb3f62076afa78963629840340a8cbfb243e33be
MagicSoft <magicsoft.uz@gmail.com>
2022-03-29 12:57:47 +0500

    Complete 237

--- script.js
+++ script.js
@@ -13,53 +13,83 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 let latitude = 0;
 let longitude = 0;
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function (e) {
+let map;
+let eventMap;
+
+class App {
+  #map;
+  #eventMap;
+  constructor() {
+    this._getPosition();
+
+    form.addEventListener('submit', this._newWorkout.bind(this));
+
+    inputType.addEventListener('change', this._toggleElevetionField);
+  }
+
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Geolocation not working!');
+        }
+      );
+    }
+  }
+
+  _loadMap(e) {
     latitude = e.coords.latitude;
     longitude = e.coords.longitude;
 
-    const map = L.map('map').setView([latitude, longitude], 18);
+    this.#map = L.map('map').setView([latitude, longitude], 18);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
-    // create a red polyline from an array of LatLng points
-    var latlngs = [
-      [latitude, longitude],
-      [latitude + 1, longitude - 1],
-      [latitude + 1, longitude - 1],
-    ];
+    }).addTo(this.#map);
 
-    map.on('click', function (e) {
-      console.log(e);
-      L.marker([e.latlng.lat, e.latlng.lng], {
-        riseOnHover: true,
-        opacity: 1,
-        draggable: true,
-      })
-        .addTo(map)
-        .bindPopup(
-          L.popup({
-            maxWidth: 300,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-          })
-            .setLatLng([e.latlng.lat, e.latlng.lng])
-            .setContent('Working')
-            .openOn(map)
-        )
-        .openPopup();
-    });
-    // zoom the map to the polyline
+    this.#map.on('click', this._showForm.bind(this));
 
-    L.circle([latitude, longitude], { radius: 20 }).addTo(map);
-  });
-} else {
-  alert('San ruxsat bermading!!');
+    L.circle([latitude, longitude], { radius: 20 }).addTo(this.#map);
+  }
+
+  _showForm(e) {
+    this.#eventMap = e;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleElevetionField() {
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+
+    L.marker([this.#eventMap.latlng.lat, this.#eventMap.latlng.lng], {
+      riseOnHover: true,
+      opacity: 0.5,
+      draggable: true,
+    })
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 50,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        }).setContent('<p>Hello world!<br</p>')
+      )
+      .openPopup();
+  }
 }
-// setTimeout(function () {
-//   console.log(latitude, longitude);
-// }, 3000);
+
+const magicMap = new App();
